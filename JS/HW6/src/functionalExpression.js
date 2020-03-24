18daf34987fd8dad2e3733629cb9f68e155621ac
KrozeRoll <krozer95@gmail.com>
2020-03-24 16:13:11 +0300

    HW6 codeBecomeBetter

--- JS/HW6/src/functionalExpression.js
+++ JS/HW6/src/functionalExpression.js
@@ -4,38 +4,40 @@ function println() {
     console.log(Array.prototype.map.call(arguments, String).join(' '));
 }
 
-const binaryOperation = operation => (op1, op2) => (x, y, z) => operation(op1(x, y, z), op2(x, y, z));
-const unaryOperation = operation => (op) => (x, y, z) => operation(op(x, y, z));
+const abstractOperation = operation => (...operands) => (x, y, z) => {
+    let operandsOfOperation = [];
+    for (let operand of operands) {
+        operandsOfOperation.push(operand(x, y, z));
+    }
+    return operation(...operandsOfOperation);
+};
 
-const cnst = value => (x, y, z) => value;
+const cnst = value => () => value;
 const variable = input => (x, y, z) => input === "x" ? x : (input === "y" ? y : z);
 
-const add = binaryOperation((a, b) => a + b);
-const subtract = binaryOperation((a, b) => a - b);
-const multiply = binaryOperation((a, b) => a * b);
-const divide = binaryOperation((a, b) => a / b);
-const negate = unaryOperation((a) => -a);
-const sin = unaryOperation((a) => Math.sin(a));
-const cos = unaryOperation((a) => Math.cos(a));
-const cube = unaryOperation((a) => Math.pow(a, 3));
-const cuberoot = unaryOperation((a) => Math.cbrt(a));
+const add = abstractOperation((a, b) => a + b);
+const subtract = abstractOperation((a, b) => a - b);
+const multiply = abstractOperation((a, b) => a * b);
+const divide = abstractOperation((a, b) => a / b);
+const negate = abstractOperation((a) => -a);
+const sin = abstractOperation((a) => Math.sin(a));
+const cos = abstractOperation((a) => Math.cos(a));
+const cube = abstractOperation((a) => Math.pow(a, 3));
+const cuberoot = abstractOperation((a) => Math.cbrt(a));
 
 const pi = cnst(Math.PI);
 const e = cnst(Math.E);
 
-let binaryOperator = {
-    "-" : subtract,
-    "+" : add,
-    "*" : multiply,
-    "/" : divide
-};
-
-let unaryOperator = {
-    "negate" : negate,
-    "sin" : sin,
-    "cos" : cos,
-    "cube" : cube,
-    "cuberoot" : cuberoot
+let operator = {
+    "-" : [subtract, 2],
+    "+" : [add, 2],
+    "*" : [multiply, 2],
+    "/" : [divide, 2],
+    "negate" : [negate, 1],
+    "sin" : [sin, 1],
+    "cos" : [cos, 1],
+    "cube" : [cube, 1],
+    "cuberoot" : [cuberoot, 1]
 };
 
 let operand = {
@@ -44,17 +46,20 @@ let operand = {
     "z" : variable("z"),
     "pi" : pi,
     "e" : e
-}
+};
 
 const parse = function(expr) {
     let stack = [];
     for (let i of expr.split(/\s/)) {
         if (i.length > 0) {
-            if (i in binaryOperator) {
-                let secondOperand = stack.pop();
-                stack.push(binaryOperator[i](stack.pop(), secondOperand));
-            } else if (i in unaryOperator) {
-                stack.push(unaryOperator[i](stack.pop()));
+            if (i in operator) {
+                let operands = [];
+                let arity = operator[i][1];
+                for (let j = 0; j < arity; j++) {
+                    operands.push(stack.pop());
+                }
+                operands.reverse();
+                stack.push(operator[i][0](...operands));
             } else if (i in operand) {
                 stack.push(operand[i]);
             } else {
@@ -65,5 +70,8 @@ const parse = function(expr) {
     return stack.pop();
 };
 
-//let expr = cuberoot(add(variable('x'), variable('y')));
+//let expr = add(variable('x'), variable('y'));
 //println(expr(228, 1, 0));
+
+//let expr = "3 2 +"
+//println(parse(expr)(2, 2, 3));
